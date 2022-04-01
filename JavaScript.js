08c00f89b2639d3df06f73fa9f90e4ffa41e660e
Daniel Kozlowski <daniel.kozlowski@vp.pl>
2022-04-01 19:36:43 +0200

    Works without bigger bugs

--- JavaScript.js
+++ JavaScript.js
@@ -13,10 +13,11 @@ let firstNum=0;
 
 let reset =0
 let addComma=0
-
+let afterEqual=0
 //Dodanie do wszystkich przyciskow liczbowych 
 butt.forEach(Num=>{
     Num.addEventListener('click',() => {
+        console.log(reset)
         if (reset==1){
             firstNum=enterNum
             enterNum=0
@@ -48,25 +49,23 @@ backspace.addEventListener('click',() =>{
     enterNum=enterNum.substr(0,enterNum.length-1);
     enterNum=Number(enterNum)
     display.textContent=enterNum;
-    
-
 })
-
-let dupa = 'dupa'
-let moko = dupa.substr(0,dupa.length-1)
-console.log(moko)
 
 calc.forEach(action=>{
     action.addEventListener('click',() => {
+        if (afterEqual==0){
         operate()
-        runn=action.textContent;           
+    }
+        runn=action.textContent;
+        afterEqual=0;           
     })
 })
 
 equal.addEventListener('click',() =>{
     operate()
-    firstNum=enterNum;
-    enterNum=0
+    afterEqual=1;
+    console.log(`first num ${firstNum}`)
+    console.log(`enter num ${enterNum}`)
 }
 )
 
@@ -74,6 +73,7 @@ function operate(a,b,run){
     a=firstNum;
     b=enterNum;
     run=runn
+    console.log(`run:${run}`)
     if (run=='+'){
         enterNum=a+b
         display.textContent=enterNum
@@ -85,6 +85,7 @@ function operate(a,b,run){
     else if (run=='*'){
         enterNum=a*b
         display.textContent=enterNum
+        
     }
     else if (run=='/'){
         if (b==0){
@@ -105,7 +106,10 @@ clear.addEventListener('click', function() {
     firstNum=0;
     enterNum=0;
     display.textContent=0;
-    addComma=0
+    addComma=0;
+    reset=0;
+    afterEqual=0;
+    runn=0;
 })
 
 
